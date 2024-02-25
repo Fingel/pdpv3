4b1c7c37f05a540efa49bbf5ca7600c8c6af2a44
Austin Riba <austin@m51.io>
2024-02-25 11:17:08 -0800

    Add styling for micro posts

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,7 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     model = Post
     list_display = ["slug", "title", "date"]
+    prepopulated_fields = {"slug": ["title"]}
 
 
 admin.site.register(Post, PostAdmin)
